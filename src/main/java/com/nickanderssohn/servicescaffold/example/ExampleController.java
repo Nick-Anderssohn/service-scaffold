8cec115263a9ebbfd64fdb8728836d79257eb30d
Nick Anderssohn <nicholas.anderssohn@outlook.com>
2021-03-07 15:41:04 -0800

    init

---
+++ src/main/java/com/nickanderssohn/servicescaffold/example/ExampleController.java
@@ -0,0 +1,13 @@
+package com.nickanderssohn.servicescaffold.example;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
---
+++ src/test/java/com/nickanderssohn/servicescaffold/ServiceScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickanderssohn.servicescaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceScaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
